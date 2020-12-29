369093b8e65e132c3d7b1b94940f353a315fa5dc
R-Mielamud <2m.roman2@gmail.com>
2020-12-29 12:23:47 +0200

    Fixed database problems

--- BooksDelivery/settings.py
+++ BooksDelivery/settings.py
@@ -78,12 +78,24 @@ WSGI_APPLICATION = 'BooksDelivery.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgres_psycopg2",
+            "NAME": "botdb",
+            "USER": "botuser",
+            "PASSWORD": os.environ.get("POSTGRES_PASSWD", ""),
+            "HOST": "127.0.0.1",
+            "PORT": "5432"
+        }
+    }
 
 
 # Password validation
