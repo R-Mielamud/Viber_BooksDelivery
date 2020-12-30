92d6e5363bdd8401440de9ede7af6d1802838753
R-Mielamud <2m.roman2@gmail.com>
2020-12-30 15:17:51 +0200

    Fixed ForeignKeys

--- bot/models.py
+++ bot/models.py
@@ -3,23 +3,23 @@ from django.db.models import *
 class Dated(Model):
     created_at = DateField(auto_now=True)
 
-class Order(Dated):
-    books = JSONField(default=list)
-
 class Requisites(Dated):
     delivery_phone = CharField(max_length=20, default="+000000000000")
     delivery_name = CharField(max_length=100, default="")
     post_service = CharField(max_length=300, default="")
     delivery_address = CharField(max_length=300, default="")
 
-class Bill(Dated):
-    amount = CharField(max_length=100, default="")
-    comment = TextField(max_length=500, default="")
-
 class ViberUser(Model):
     viber_id = CharField(max_length=50, default="1234567890A==")
     phone = CharField(max_length=20, blank=True, null=True)
-    orders = ForeignKey(Order, on_delete=CASCADE, related_name="user", default=[])
     requisites = OneToOneField(Requisites, on_delete=CASCADE, blank=True, null=True)
-    bills = ForeignKey(Bill, on_delete=CASCADE, related_name="user", default=[])
     convers_answers_data = JSONField(default=dict)
+
+class Order(Dated):
+    books = JSONField(default=list)
+    user = ForeignKey(to=ViberUser, on_delete=CASCADE, related_name="orders", blank=True, null=True)
+
+class Bill(Dated):
+    amount = CharField(max_length=100, default="")
+    comment = TextField(max_length=500, default="")
+    user = ForeignKey(to=ViberUser, on_delete=CASCADE, related_name="bills", blank=True, null=True)
