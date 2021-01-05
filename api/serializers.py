1f7d926352e014203b11bdacbf1b4de0f25b8430
R-Mielamud <2m.roman2@gmail.com>
2021-01-05 20:42:12 +0200

    Fixed API and discovered new serializer kind

--- api/serializers.py
+++ api/serializers.py
@@ -1,4 +1,4 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, PrimaryKeyRelatedField
 from bot.models import ViberUser, Order, Requisites, Bill
 from BooksDelivery.model_fields.json import OrderedJSONFieldSerializer
 
@@ -12,7 +12,7 @@ class RequisitesSerializer(ModelSerializer):
         model = Requisites
         fields = "__all__"
 
-class UserSerializer(ModelSerializer):
+class GetUserSerializer(ModelSerializer):
     requisites = RequisitesSerializer(allow_null=True, required=False)
     convers_answers_data = OrderedJSONFieldSerializer(required=False)
 
@@ -20,15 +20,37 @@ class UserSerializer(ModelSerializer):
         model = ViberUser
         fields = "__all__"
 
-class OrderSerializer(ModelSerializer):
+class ModUserSerializer(ModelSerializer):
+    requisites = PrimaryKeyRelatedField(allow_null=True, required=False, queryset=Requisites.objects.all())
+    convers_answers_data = OrderedJSONFieldSerializer(required=False)
+
+    class Meta:
+        model = ViberUser
+        fields = "__all__"
+
+class GetOrderSerializer(ModelSerializer):
     user = MinimalUserSerializer()
 
     class Meta:
         model = Order
         fields = "__all__"
 
-class BillSerializer(ModelSerializer):
+class ModOrderSerializer(ModelSerializer):
+    user = PrimaryKeyRelatedField(queryset=ViberUser.objects.all())
+
+    class Meta:
+        model = Order
+        fields = "__all__"
+
+class GetBillSerializer(ModelSerializer):
     user = MinimalUserSerializer()
+
+    class Meta:
+        model = Bill
+        fields = "__all__"
+
+class ModBillSerializer(ModelSerializer):
+    user = PrimaryKeyRelatedField(queryset=ViberUser.objects.all())
 
     class Meta:
         model = Bill
--- api/views.py
+++ api/views.py
@@ -2,68 +2,44 @@ from django.http import JsonResponse
 from django.views import View
 from rest_framework.viewsets import ModelViewSet
 from bot.models import ViberUser, Order, Requisites, Bill
-from api.serializers import UserSerializer, OrderSerializer, RequisitesSerializer, BillSerializer
 
-class UserDependingAPIView(ModelViewSet):
-    def user_including_request(self, request, action):
-        data = request.data
-        user_id = data.pop("user", None)
-        user = ViberUser.objects.filter(pk=user_id).first()
-        result = action(user, data)
-        serializer = self.serializer_class(result)
-        return JsonResponse(serializer.data)
+from api.serializers import (
+    GetUserSerializer, ModUserSerializer,
+    GetOrderSerializer, ModOrderSerializer,
+    GetBillSerializer, ModBillSerializer,
+    RequisitesSerializer
+)
 
 class UserAPIView(ModelViewSet):
     queryset = ViberUser.objects.all()
-    serializer_class = UserSerializer
 
-class OrderAPIView(UserDependingAPIView):
+    def get_serializer_class(self):
+        if self.action == "list" or self.action == "retrieve":
+            return GetUserSerializer
+        else:
+            return ModUserSerializer
+
+class OrderAPIView(ModelViewSet):
     queryset = Order.objects.all()
-    serializer_class = OrderSerializer
 
-    def create(self, request):
-        action = lambda user, data: self.queryset.create(**data, user=user)
-        return self.user_including_request(request, action)
-
-    def get_update_action(self, pk):
-        def action(user, data):
-            order_set = self.queryset.filter(pk=pk)
-            order_set.update(**data, user=user)
-            return order_set.first()
-
-        return action
-
-    def update(self, request, pk):
-        return self.user_including_request(request, self.get_update_action(pk))
-
-    def partial_update(self, request, pk, **kwargs):
-        return self.user_including_request(request, self.get_update_action(pk))
+    def get_serializer_class(self):
+        if self.action == "list" or self.action == "retrieve":
+            return GetOrderSerializer
+        else:
+            return ModOrderSerializer
 
 class RequisitesAPIView(ModelViewSet):
     queryset = Requisites.objects.all()
     serializer_class = RequisitesSerializer
 
-class BillAPIView(UserDependingAPIView):
+class BillAPIView(ModelViewSet):
     queryset = Bill.objects.all()
-    serializer_class = BillSerializer
 
-    def create(self, request):
-        action = lambda user, data: self.queryset.create(**data, user=user)
-        return self.user_including_request(request, action)
-
-    def get_update_action(self, pk):
-        def action(user, data):
-            bill_set = self.queryset.filter(pk=pk)
-            bill_set.update(**data, user=user)
-            return bill_set.first()
-
-        return action
-
-    def update(self, request, pk):
-        return self.user_including_request(request, self.get_update_action(pk))
-
-    def partial_update(self, request, pk, **kwargs):
-        return self.user_including_request(request, self.get_update_action(pk))
+    def get_serializer_class(self):
+        if self.action == "list" or self.action == "retrieve":
+            return GetBillSerializer
+        else:
+            return ModBillSerializer
 
 class GetUserByMessengerIdAPI(View):
     def get(self, request, **kwargs):
@@ -75,5 +51,5 @@ class GetUserByMessengerIdAPI(View):
                 "detail": "Not found."
             }, status=404)
 
-        serializer = UserSerializer(user)
+        serializer = GetUserSerializer(user)
         return JsonResponse(serializer.data)
