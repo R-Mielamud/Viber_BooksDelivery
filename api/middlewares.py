de8d2974b1d0c00cd8dda8872b0717c2de263516
R-Mielamud <2m.roman2@gmail.com>
2021-01-23 16:10:46 +0200

    Fixed middleware

--- api/middlewares.py
+++ api/middlewares.py
@@ -5,12 +5,12 @@ def check_api_key_middleware(get_response):
     def middleware(request):
         if request.path.startswith("/api"):
             authorization = request.headers.get("Authorization")
-            
+
             if authorization != settings.API_KEY:
                 return JsonResponse({
                     "message": "Invalid credential",
                 }, status=403)
 
-            return get_response(request)
+        return get_response(request)
 
     return middleware
